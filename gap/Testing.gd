dc08db33647c87406e0b2b5969cd289c13858465
Samuel Wilson <yokomeshi@gmail.com>
2021-10-29 20:30:47 -0500

    Texify

--- gap/Testing.gd
+++ gap/Testing.gd
@@ -14,13 +14,13 @@ DeclareGlobalFunction( "SL2Reps_SL2Conj" );
 
 #! @Arguments S,T,p,ld
 #! @Returns a list representing a character of $\mathrm{SL}_2(\mathbb{Z}/p^\lambda\mathbb{Z})$
-#! @Description Converts the modular data (S,T), which must have level dividing p^ld,
+#! @Description Converts the modular data $(S,T)$, which must have level dividing $p^\lambda$,
 #! into a character of $\mathrm{SL}_2(\mathbb{Z}/p^\lambda\mathbb{Z})$, presented in a
 #! form matching the conjugacy classes used in `SL2Reps_SL2Conj`.
 DeclareGlobalFunction( "SL2Reps_ChiST" );
 
 #! @Arguments p, lambda
 #! @Returns a boolean
-#! @Description Constructs and tests all irreps of level dividing p^ld by checking their
-#! positions in Irr(G).
+#! @Description Constructs and tests all irreps of level dividing $p^\lambda$ by checking their
+#! positions in `Irr(G)`.
 DeclareGlobalFunction( "SL2Reps_IrrepPositionTest" );
